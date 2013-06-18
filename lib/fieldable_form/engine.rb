68b7da6772f4f6b61f8c9316d3f9addc4a1b4144
Roy Y. Bao <roybao2010@gmail.com>
2013-06-18 14:18:21 -0400

    Rails4 dosnt like the '+' method

--- lib/fieldable_form/engine.rb
+++ lib/fieldable_form/engine.rb
@@ -1,7 +1,7 @@
 module FieldableForm
   class Engine < ::Rails::Engine
     initializer 'fieldable_form.add_migration_paths' do |app|
-      app.config.paths['db/migrate'] += FieldableForm::Engine.paths['db/migrate'].existent
+      app.config.paths['db/migrate'] << FieldableForm::Engine.paths['db/migrate'].existent
     end
   end
 end
